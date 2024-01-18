f69a63a41a032ca7dad41f76f3519d1be088b255
srihariraj <srihariatom@gmail.com>
2024-01-18 14:45:35 +0530

    Second try

--- src/main/java/com/Project/SpringWeb/UserManagementApplication.java
+++ src/main/java/com/Project/SpringWeb/UserManagementApplication.java
@@ -9,7 +9,7 @@ public class UserManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementApplication.class, args);
 
-		System.out.println("App Started Running");
+		
 	}
 
 }
